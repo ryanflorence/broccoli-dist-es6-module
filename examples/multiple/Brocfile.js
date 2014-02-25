256536d768d06a72aa1c6ab5b1aa8d0bd6db934f
Ryan Florence <rpflorence@gmail.com>
2014-02-25 15:06:11 -0700

    upgrade module-filter

--- examples/multiple/Brocfile.js
+++ examples/multiple/Brocfile.js
@@ -2,12 +2,12 @@ var makeES6Module = require('../../index');
 
 module.exports = function(broccoli) {
   return makeES6Module(broccoli.makeTree('lib'), {
-    global: 'myNamespace',
     main: 'main',
-    packageName: 'fake-lib',
+    packageName: 'lib',
+    namespace: 'lib',
     imports: {
       'jquery': 'jQuery',
-      './foo': 'myNamespace.foo'
+      './foo': 'lib.foo'
     }
   });
 };
