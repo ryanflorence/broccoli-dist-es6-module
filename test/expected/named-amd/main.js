4cb11b4d9952880ada160a9bba89c0b639015f8a
Ryan Florence <rpflorence@gmail.com>
2014-02-27 12:44:22 -0700

    fix expected test

--- test/expected/named-amd/main.js
+++ test/expected/named-amd/main.js
@@ -6,18 +6,14 @@ define("arithmetic",
     var product = __dependency2__["default"] || __dependency2__;
     __exports__.sum = sum;
     __exports__.product = product;
-  });
-
-define("arithmetic/product",
+  });define("arithmetic/product",
   ["exports"],
   function(__exports__) {
     "use strict";
     __exports__["default"] = function(x, y) {
       return x * y;
     };
-  });
-
-define("arithmetic/sum",
+  });define("arithmetic/sum",
   ["exports"],
   function(__exports__) {
     "use strict";
